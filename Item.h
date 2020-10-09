ba6b7a99cc73216d81812b41a2b2414fe44bd28c
= <jeka.sudakov@mail.ru>
2020-10-09 21:52:54 +0300

    build script + interface in Item.h

--- Item.h
+++ Item.h
@@ -8,8 +8,19 @@
 //Интерфейс объекта поля (монетка или буст)
 class Item {
 public:
-    virtual void create();
+    virtual ~Item() {};
+    virtual void create() = 0;
 };
 
+class Coin : public Item {
+public:
+    void create() override {
+        return;
+    }
+
+    ~Coin() override {
+
+    }
+};
 
 #endif //OOP_ITEM_H
---
+++ start.sh
@@ -0,0 +1,2 @@
+g++ Cell.cpp Cell.h Field.cpp Field.h Game.cpp Game.h Item.cpp Item.h main.cpp Player.cpp Player.h -lsfml-graphics -lsfml-window -lsfml-system -o prog
+./prog
